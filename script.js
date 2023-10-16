72eb076ddb0e324bbef3378f864b6124fb10d74a
shuppet33 <118700008+shuppet33@users.noreply.github.com>
2023-10-16 19:06:39 +0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,10 @@
+
+$('.main-slider').slick({
+    infinite: true,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    ariableWidth: true,
+    adaptiveHeight: true,
+    autoplay: true,
+    autoplaySpeed: 2000,
+  });
